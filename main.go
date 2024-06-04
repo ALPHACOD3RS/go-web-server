c5807058255a55ceaef213d9f9da7a5ffa2ba773
Naol Ketema <naolketema55@gmail.com>
2024-06-05 00:23:59 +0300

    some boiler plate

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -24,18 +25,40 @@ func main(){
 
 	router := chi.NewRouter()
 
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET", "POST", "PUT", "DELTET", "OPTIONS"},
+		AllowedHeaders: []string{"*"},
+		ExposedHeaders: []string{"Link"},
+		AllowCredentials: false,
+		MaxAge: 300,
+	}))
+
+	v1Router := chi.NewRouter()
+
+	v1Router.Get("/health", handlerReadines)
+	v1Router.Get("/error", handlerError)
+
+	router.Mount("/v1", v1Router)
+
+
+
+
 	srver := &http.Server{
 		Handler: router,
 		Addr: ":" + portString,
 	}
+
+
+	log.Printf("The server is runing in port %v", portString);
+
+
 
 	err := srver.ListenAndServe()
 
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	print(srver)
 
 	fmt.Println(portString)
 }
